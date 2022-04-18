e6cd63f0d38322d7ee2218eb0feb1007d086cbc2
Jessada Srimoon <jessada_sr@hotmail.com>
2022-04-18 23:02:35 +0700

    config : PORT and Uri for Vercel.

---
+++ api/index.js
@@ -0,0 +1,37 @@
+// import express
+const express = require('express');
+
+// import body-parser for receive request's input data
+const bodyParser = require('body-parser');
+
+// import cors
+const cors = require('cors');
+
+//import external routers
+const userRouter = require('../src/routes/user');
+
+//import mongoose 
+const mongoose = require('mongoose');
+
+// PORT config
+const PORT = process.env.PORT || 4001;
+
+// import config
+const config =require('../src/scripts/config')
+
+// define express instance for use it in this server
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+    cors({
+      origin: '*',
+      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    })
+  );
+
+app.use('/users', userRouter);
+//refactor with express router 
+// if request's path is matched with this path ('/users/:userId') the request will be flow to user.js 
+
+module.exports = app;
--- src/app.js
+++ src/app.js
@@ -1,50 +1,19 @@
-// import express
-const express = require('express');
-
-// import body-parser for receive request's input data
-const bodyParser = require('body-parser');
-
-// import cors
-const cors = require('cors');
-
-//import external routers
-const userRouter = require('./routes/user');
-
-//import mongoose 
 const mongoose = require('mongoose');
+const app = require('../api/index');
 
-// PORT config
-const PORT = process.env.PORT || 4001;
-
-// import config
-const config =require('./scripts/config')
-
-// define express instance for use it in this server
-const app = express();
-
-app.use(bodyParser.json());
-app.use(
-    cors({
-      origin: '*',
-      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-    })
-  );
-
-app.use('/users', userRouter);
-//refactor with express router 
-// if request's path is matched with this path ('/users/:userId') the request will be flow to user.js 
+const config = require('./scripts/config');
 
 //start server
 const boot = async () => {
-    // Connect to mongodb
-    await mongoose.connect(config);
-    // Start express server
-    app.listen(PORT, () => {
-      console.log(`Server is listening on port ${PORT} `);
-    });
-  };
-  
-  boot();
+  // Connect to mongodb
+  await mongoose.connect(config.mongoUri, config.mongoOptions);
+  // Start express server
+  app.listen(config.port, () => {
+    console.log(`Server is listening on port ${config.port}`);
+  });
+};
+
+boot();
 
 
 
